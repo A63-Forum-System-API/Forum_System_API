bb9664b903a01827d3de057b1345644e99563208
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-10-07 18:18:48 +0300

    Add Reply Pydantic model

---
+++ schemas/reply.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing_extensions import Optional
+
+
+class Topic(BaseModel):
+    id: Optional[int]
+    content: str
+    is_best_reply: bool = False
+    created_at: datetime = datetime.now()
+    topic_id: int
+    author_id: int
+
+    @classmethod
+    def from_query_result(cls, id, content,
+                          is_best_reply, created_at):
+        return cls(id=id,
+                   content=content,
+                   is_best_reply=is_best_reply,
+                   created_at=created_at)
