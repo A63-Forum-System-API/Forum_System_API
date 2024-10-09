915372ef9876e55ae5316c41f01ae3d269cec7ad
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-10-09 09:55:55 +0300

    Add Vote Pydantic model to schemas

---
+++ schemas/vote.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing_extensions import Optional
+
+
+class Vote(BaseModel):
+    id: Optional[int]
+    up_vote: bool = False
+    down_vote: bool = False
+    reply_id: int
+    user_id: int
+
+    @classmethod
+    def from_query_result(cls, id, up_vote, down_vote, reply_id, user_id):
+        return cls(id=id,
+                   up_vote=up_vote,
+                   down_vote=down_vote,
+                   reply_id=reply_id,
+                   user_id=user_id)
+
