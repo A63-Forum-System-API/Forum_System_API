b6ef7ea710f938a2322c3c0ddd835c85424c90e1
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-10-12 21:34:53 +0300

    Add more models for Topic

--- schemas/topic.py
+++ schemas/topic.py
@@ -1,27 +1,49 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
+from schemas.reply import Reply
 
-class Topic(BaseModel):
-    id: Optional[int]
+
+class TopicBase(BaseModel):
     title: str
-    content: str
-    is_locked: bool = False
-    created_at: datetime = datetime.now()
+    is_locked: bool
     category_id: int
     author_id: int
-    best_reply_id: Optional[int]
 
-    @classmethod
-    def from_query_result(cls, id, content,
-                          is_locked, created_at, category_id,
-                          author_id, best_reply_id):
-        return cls(id=id,
-                   content=content,
-                   is_locked=is_locked,
-                   created_at=created_at,
-                   category_id=category_id,
-                   author_id=author_id,
-                   best_reply_id=best_reply_id)
+    class Config:
+        fields = {'title': 0, 'is_locked': 1, 'category_id': 2, 'author_id': 3}
+
+class TopicsView(TopicBase):
+    id: int
+    created_at: datetime
+    replies_count: int
+
+    class Config:
+        fields = {'id': 0, 'title': 1, 'content': 2, 'is_locked': 3,
+                  'created_at': 4, 'category_id': 5, 'author_id': 6,
+                  'best_reply_id': 7, 'replies_count': 8, 'all_replies': 9}
+
+class TopicView(TopicBase):
+    id: int
+    content: str
+    created_at: datetime
+    best_reply_id: Optional[int]
+    replies_count: int
+    all_replies: list[Reply]
+
+    class Config:
+        fields = {'id': 0, 'title': 1, 'is_locked': 2, 'created_at': 3,
+                  'category_id': 4, 'author_id': 5, 'replies_count': 6}
+
+class TopicCreate(TopicBase):
+    content: str
+
+    class Config:
+        fields = {'title': 0, 'content': 1, 'is_locked': 2,
+                  'category_id': 3, 'author_id': 4}
+
+
+class TopicUpdate(BaseModel):
+    is_locked: bool
 
