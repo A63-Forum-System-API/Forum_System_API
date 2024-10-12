15d2a91d97e0cf1dc10a519848fb6e817f3d0bd0
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-10-12 23:20:46 +0300

    Add app.include_router for topics_router in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers.categories import categories_router
-
+from routers.topics import topics_router
 
 app = FastAPI()
 app.include_router(categories_router)
+app.include_router(topics_router)
