6f45dee8bfe6e6960ab1e1f12f3ee5c9fe3e0e24
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-10-13 00:09:10 +0300

    Fix forum_system_structure

--- forum_system_structure.sql
+++ forum_system_structure.sql
@@ -25,17 +25,17 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(20) NOT NULL,
-  `description` varchar(100) DEFAULT NULL,
-  `is_private` tinyint(4) NOT NULL DEFAULT 0,
-  `is_locked` tinyint(4) NOT NULL DEFAULT 0,
-  `created_at` datetime DEFAULT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` text DEFAULT NULL,
+  `is_private` tinyint(1) NOT NULL DEFAULT 0,
+  `is_locked` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `admin_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`admin_id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_categories_users1_idx` (`admin_id`),
   CONSTRAINT `fk_categories_users1` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,8 +48,8 @@ DROP TABLE IF EXISTS `category_accesses`;
 CREATE TABLE `category_accesses` (
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `write_access` tinyint(4) DEFAULT 0,
-  `read_access` tinyint(4) DEFAULT 1,
+  `write_access` tinyint(1) NOT NULL DEFAULT 0,
+  `read_access` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`user_id`,`category_id`),
   KEY `fk_users_has_categories_categories1_idx` (`category_id`),
   KEY `fk_users_has_categories_users_idx` (`user_id`),
@@ -87,8 +87,8 @@ DROP TABLE IF EXISTS `messages`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `text` varchar(45) NOT NULL,
-  `sent_at` date DEFAULT NULL,
+  `text` text NOT NULL,
+  `sent_at` datetime NOT NULL DEFAULT current_timestamp(),
   `sender_id` int(11) NOT NULL,
   `receiver_id` int(11) NOT NULL,
   `conversation_id` int(11) NOT NULL,
@@ -113,8 +113,8 @@ DROP TABLE IF EXISTS `replies`;
 CREATE TABLE `replies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
-  `is_best_reply` tinyint(4) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
+  `is_best_reply` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `topic_id` int(11) NOT NULL,
   `author_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`topic_id`,`author_id`),
@@ -123,7 +123,7 @@ CREATE TABLE `replies` (
   KEY `fk_replies_users1_idx` (`author_id`),
   CONSTRAINT `fk_replies_topics1` FOREIGN KEY (`topic_id`) REFERENCES `topics` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_replies_users1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,12 +137,12 @@ CREATE TABLE `topics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
   `content` text NOT NULL,
-  `is_locked` tinyint(4) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
+  `is_locked` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `category_id` int(11) NOT NULL,
   `author_id` int(11) NOT NULL,
-  `best_reply_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`category_id`,`author_id`,`best_reply_id`),
+  `best_reply_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`category_id`,`author_id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_topics_categories1_idx` (`category_id`),
   KEY `fk_topics_users1_idx` (`author_id`),
@@ -150,7 +150,7 @@ CREATE TABLE `topics` (
   CONSTRAINT `fk_topics_categories1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_topics_replies1` FOREIGN KEY (`best_reply_id`) REFERENCES `replies` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_topics_users1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,19 +162,19 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `hash_password` varchar(20) NOT NULL,
-  `is_admin` tinyint(4) DEFAULT 0,
-  `created_at` datetime DEFAULT NULL,
-  `picture` varchar(45) DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `hash_password` varchar(255) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `picture` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `idusers_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,8 +186,7 @@ DROP TABLE IF EXISTS `votes`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `votes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `up_vote` tinyint(4) DEFAULT NULL,
-  `down_vote` tinyint(4) DEFAULT NULL,
+  `vote_type` tinyint(1) NOT NULL,
   `reply_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`reply_id`,`user_id`),
@@ -208,4 +207,4 @@ CREATE TABLE `votes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2024-10-09  9:36:31
+-- Dump completed on 2024-10-13  0:07:44
