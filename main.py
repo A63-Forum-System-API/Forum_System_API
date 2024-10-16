79aa579ca457cd1cbcb57939aa39cdf798ec9e7f
Rumyana <rummiev98@gmail.com>
2024-10-16 17:32:54 +0300

    add register and login endpoints

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import uvicorn
 from fastapi import FastAPI
 from routers.categories import categories_router
 from routers.replies import replies_router
+from routers.tokens import token_router
 from routers.topics import topics_router
+from routers.users import users_router
 
 app = FastAPI()
 app.include_router(categories_router)
 app.include_router(topics_router)
 app.include_router(replies_router)
-
+app.include_router(token_router)
+app.include_router(users_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='localhost', port=8000)
