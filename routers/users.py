8fdd74900f7212c837d98473521c94d67693d41b
Rumyana <rummiev98@gmail.com>
2024-10-17 22:52:59 +0300

    user update status added

--- routers/users.py
+++ routers/users.py
@@ -1,23 +1,40 @@
-from fastapi import APIRouter, Depends
+import mariadb
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.openapi.models import Response
 from fastapi.security import OAuth2PasswordRequestForm
-
+from starlette import status
+from starlette.responses import JSONResponse
 from common.auth import authenticate_user, create_access_token
 from schemas.reply import ReplyBase
 from schemas.token import Token
-from schemas.user import UserCreate, UserLogIn
+from schemas.user import UserCreate, UserLogIn, UserUpdate, UserUpdate
 from services import topic_service, reply_service, user_service
+from common.auth import get_current_user
 
 users_router = APIRouter(prefix='/users')
 
 
 @users_router.post('/register', status_code=201)
 def create_user(user: UserCreate):
-    existing_user = user_service.get_by_username(user.username)
-    if existing_user:
-        return Response(content=f"User with username {user.username} already exists", status_code=409)
+    try:
+        return user_service.create(user)
 
-    return user_service.create(user)
+    except mariadb.IntegrityError as e:
+        if "email_UNIQUE" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"User with email '{user.email}' already exists!"
+            )
+        elif "username_UNIQUE" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"User with username '{user.username}' already exists!"
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An unexpected error occurred!"
+            )
 
 
 @users_router.post('/login')
@@ -25,9 +42,42 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(form_data.username, form_data.password)
 
     if not user:
-        return Response(content="User not found", status_code=404)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found!"
+        )
 
     access_token = create_access_token(data=user)
     return Token(access_token=access_token,
                  token_type='bearer')
+
+
+@users_router.put('/{user_id}')
+def update_user(user_id: int, user: UserUpdate, current_user_id: int = Depends(get_current_user)):
+    if not user_service.is_admin(current_user_id):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not allowed to update this user!"
+        )
+
+    if not user_service.id_exists(user_id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with ID {user_id} not found!"
+        )
+
+    if user_id == current_user_id and user_id != 1:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not allowed to update your own user status!"
+        )
+
+    if user_service.is_admin(user_id) and current_user_id != 1:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not allowed to update an admin user to a regular user!"
+        )
+
+    return user_service.update(user_id, user)
+
 
--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from fastapi import HTTPException
 from pydantic import BaseModel, Field, field_validator
 from typing import Optional
@@ -9,29 +8,29 @@ from starlette import status
 
 
 class User(BaseModel):
-    # id: Optional[int]
     username: str = Field(
         min_length=5,
         max_length=15,
         pattern="^[a-zA-Z0-9_-]+$",
-        example="example_user"
+        examples=["example_user"]
     )
     first_name: str = Field(
         min_length=2,
         max_length=25,
-        pattern="^[a-zA-Z]+$",
-        example="Example"
+        pattern="^[a-zA-Z]+(?:-[a-zA-Z]+)?$",
+        examples=["Example"]
     )
     last_name: str = Field(
         min_length=2,
         max_length=25,
-        pattern="^[a-zA-Z]+$",
-        example="Example"
+        pattern="^[a-zA-Z]+(?:-[a-zA-Z]+)?$",
+        examples=["Example"]
     )
     email: EmailStr
-    is_admin: bool = False
-    created_at: datetime = Field(default_factory=datetime.now)
-    picture: Optional[str] = None
+    picture: Optional[str] = Field(
+        None,
+        examples=["https://example.com/user-picture.png"]
+    )
 
     @field_validator('first_name', 'last_name', mode='before')
     def capitalize_names(cls, value):
@@ -42,7 +41,7 @@ class UserCreate(User):
     password: str = Field(
         min_length=4,
         max_length=36,
-        example="Example123@"
+        examples=["Example123@"]
     )
 
     @field_validator('password')
@@ -51,18 +50,23 @@ class UserCreate(User):
                 any(c.isupper() for c in pwd) and
                 any(c.islower() for c in pwd) and
                 any(c.isdigit() for c in pwd) and
-                any(c in '@$!%*?&' for c in pwd)
+                any(c in '@$!%*?&' for c in pwd) and
+                not any(c.isspace() for c in pwd)
         )
 
         if not validate(value):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Password must contain at least one uppercase letter, one lowercase letter, "
-                       "one number, and one special character"
+                       "one number, and one special character and must not contain any spaces"
             )
         return value
 
 
-class UserLogIn(User):
+class UserLogIn(BaseModel):
     username: str
     password: str
+
+
+class UserUpdate(BaseModel):
+    is_admin: bool
