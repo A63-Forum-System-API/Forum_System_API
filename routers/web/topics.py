3169d51e34e2bfbc76668c3fae559f815e60abec
Rumyana <rummiev98@gmail.com>
2024-11-09 13:39:12 +0200

    small changes made

--- routers/web/topics.py
+++ routers/web/topics.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
@@ -50,4 +50,109 @@ def get_topic_by_id(request: Request, topic_id: int):
             context={
                 "error": "Oops! Something went wrong while loading conversations 🙈",
             }
+        )
+
+@topics_router.get("/")
+def get_all_topics(
+        request: Request,
+        search: str | None = None,
+        category_id: str | None = None,
+        author_id: str | None = None,
+):
+    try:
+        token = request.cookies.get("token")
+        if not token:
+            return RedirectResponse(
+                url="/?error=not_authorized_categories", status_code=302
+            )
+
+        try:
+            current_user_id = get_current_user(token)
+            if author_id == "true":
+                author_id = current_user_id
+
+        except:
+            return RedirectResponse(url="/?error=invalid_token", status_code=302)
+
+        if category_id is not None:
+            category_id_int = int(category_id)
+            category = category_service.get_by_id(category_id_int)
+            category_title = category.title
+        else:
+            category_id_int = None
+            category_title = None
+
+        topics = topic_service.get_all_topics(
+            user_id=current_user_id,
+            author_id=author_id,
+            search=search,
+            category_id=category_id_int,
+            limit=100,
+            offset=0,
+            is_locked=None,
+        )
+
+        authors_of_topics = topic_service.get_authors_of_topics(topics)
+        categories_of_topics = topic_service.get_categories_of_topics(topics)
+
+        return templates.TemplateResponse(
+            name="newest-topics.html",
+            context={
+                "request": request,
+                "topics": topics,
+                "categories_of_topics": categories_of_topics,
+                "authors_of_topics": authors_of_topics,
+                "category_id": category_id,
+                "category_title": category_title,
+                "search": search,
+            },
+        )
+
+    except Exception:
+        return templates.TemplateResponse(
+            name="newest-topics.html",
+            context={
+                "request": request,
+                "error": "Oops! Something went wrong while loading topics 🙈",
+            },
+        )
+
+
+@topics_router.post("/")
+def create_topic(
+        request: Request,
+        title: str = Form(...),
+        content: str = Form(...),
+        category_id: int = Form(...),
+):
+    try:
+        token = request.cookies.get("token")
+        if not token:
+            return RedirectResponse(
+                url="/?error=not_authorized_categories", status_code=302
+            )
+
+        try:
+            current_user_id = get_current_user(token)
+
+        except:
+            return RedirectResponse(url="/?error=invalid_token", status_code=302)
+
+        topic = CreateTopicRequest(
+            title=title, content=content, is_locked=False, category_id=category_id
+        )
+
+        topic_service.create(topic, current_user_id)
+
+        return RedirectResponse(
+            url=f"/topics?category_id={category_id}", status_code=302
+        )
+
+    except Exception:
+        return templates.TemplateResponse(
+            name="newest-topics.html",
+            context={
+                "request": request,
+                "error": "Oops! Something went wrong while creating topic 🙈",
+            },
         )
